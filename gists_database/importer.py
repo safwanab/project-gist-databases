f9fb1dca8f5c4b15c740007bbf938b39eb26eb37
safwanab <safwan.a.bhuiyan@gmail.com>
2019-07-28 23:33:46 +0000

    Safwanab GISTS Database Attempt 1

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,4 +1,17 @@
 import requests
-
+import sqlite3
+​
 def import_gists_to_database(db, username, commit=True):
-    pass
+    api_url = 'https://api.github.com/users/{}/gists'.format(username)
+    response = requests.get(api_url)
+    
+    if response.status_code == 404:
+        raise requests.HTTPError()
+    
+    for gist in response.json():
+         cursor = db.execute('''
+         INSERT INTO gists
+             (github_id, html_url, git_pull_url, git_push_url, commits_url, forks_url, public, created_at, updated_at, comments, comments_url)
+         VALUES
+             (:id, :html_url, :git_pull_url, :git_push_url, :commits_url, :forks_url, :public, :created_at, :updated_at, :comments, :comments_url)
+         ''', gist)
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,19 @@
 from .models import Gist
-
+import sqlite3
+from datetime import datetime
+​
 def search_gists(db_connection, **kwargs):
-    pass
+    query = '''
+        SELECT * FROM gists '''
+    
+    for kwarg, value in kwargs.items():
+        if isinstance(value, datetime):
+            query = query + 'WHERE datetime({var}) == datetime(:{var})'.format(var=kwarg)
+        else:
+            query = query + 'WHERE {var} = :{var}'.format(var=kwarg)
+    cursor = db_connection.execute(query, kwargs)
+    results = [Gist(gist) for gist in cursor]
+    return results
+
+
+
